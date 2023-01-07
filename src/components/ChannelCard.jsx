6960951857b8ab742c340857221f8029468a2a1e
Madaminov <muhammadanasxon6694@gmail.com>
2023-01-07 19:08:22 +0500

    add

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: 'none',
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: { xs: '356px', md: '320px' },
+      height: '326px',
+      margin: 'auto',
+      marginTop,
+    }}
+  >
+
+  </Box>
+);
+
+export default ChannelCard;
