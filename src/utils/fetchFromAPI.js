428540b6c8481d7a0cee53af88986c7ca686a9f6
Madaminov <muhammadanasxon6694@gmail.com>
2023-01-08 16:05:30 +0500

    ADD

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -5,6 +5,8 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 const options = {
   params: {
     maxResults: 50,
+    part: 'snippet',
+    videoId: 'M7FIvfx5J10'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
